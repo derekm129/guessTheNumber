746ea78b92df8025fb79fc04389ffd6fd3acfc55
dmod129 <derek.modugno@gmail.com>
2022-08-09 13:44:49 -0500

    guessTheNumber

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -4,7 +4,7 @@ import random
 secretNumber = random.randint(1, 10)
 print('I am thinking of a number between 1 and 10.')
 
-# Ask the player to guess 6 times.
+# Ask the player to guess 3 times.
 for guessesTaken in range(1, 4):
     print('Take a guess.')
     guess = int(input())
